a0ae80de4c2b5122be73f71be1f7854379dc28a7
Nihilumbra03 <62447396+Nihilumbra03@users.noreply.github.com>
2020-03-30 20:50:34 +0300

    Phiztekh uploaded

---
+++ Phizteh.pas
@@ -0,0 +1,19 @@
+var
+ s : string[6];
+ i,n,x : longint;
+begin
+ readln(x);
+ n:=0;
+ for i:=100000 to 999999 do
+  begin
+   str(i,s);
+   if (pos('0',s)=0) and (pos('7',s)=0) and (pos('8',s)=0) and (pos('9',s)=0) then
+    inc(n);
+   if n=x then
+   begin
+    writeln(i);
+    break
+   end;
+  end;
+ readln;
+end.
